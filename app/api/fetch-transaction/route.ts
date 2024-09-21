c98ec1efba00fd8d91ec9811a05d7395fa995f86
jaydip676 <jaydip.lamprostech@gmail.com>
2024-09-22 04:08:52 +0800

    bug fixes: change the type for api request

--- app/api/fetch-transaction/route.ts
+++ app/api/fetch-transaction/route.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest) {
+export async function GET(req: Request) {
   if (!req.url) {
     return NextResponse.json(
       { message: "URL is not defined" },
